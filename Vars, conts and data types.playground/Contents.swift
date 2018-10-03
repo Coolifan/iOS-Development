29da002a85bd71a2258f85454a46fd64aabd1944
Yifan Li <yl506@duke.edu>
2018-10-02 23:09:59 -0400

    initial commit

---
+++ Vars, conts and data types.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myAge: Int = 22
+myAge = 23
+
+let myName: String = "Yifan"
+let myAgeInTenYears = myAge + 10
+
+let myDetails = "\(myName), \(myAge)"
+let wholeNumbers : Int = 12
+let text: String = "abc"
+
+let booleans : Bool = true
+let floatingNumber : Float = 1.32
+let double : Double = 3.33333333333
